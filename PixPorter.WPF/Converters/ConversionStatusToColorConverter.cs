26b28b526d796d459e40e45c5a27cae549a59270
GSC <gsc.code@gmail.com>
2026-02-18 12:31:47 +0100

    altered theme colors

--- PixPorter.WPF/Converters/ConversionStatusToColorConverter.cs
+++ PixPorter.WPF/Converters/ConversionStatusToColorConverter.cs
@@ -1,5 +1,6 @@
 ﻿using PixPorter.WPF.ViewModels;
 using System.Globalization;
+using System.Windows;
 using System.Windows.Data;
 using System.Windows.Media;
 
@@ -9,20 +10,25 @@ public class ConversionStatusToColorConverter : IValueConverter
 {
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
-        if (value is ConversionStatus status)
-        {
-            return status switch
-            {
-                ConversionStatus.Done => new SolidColorBrush(Color.FromRgb(34, 197, 94)),
-                ConversionStatus.Failed => new SolidColorBrush(Color.FromRgb(255, 68, 68)),
-                ConversionStatus.Converting => new SolidColorBrush(Color.FromRgb(160, 160, 160)),
-                _ => new SolidColorBrush(Color.FromRgb(100, 100, 100))
-            };
-        }
+        if (value is not ConversionStatus status)
+            return new SolidColorBrush(Colors.Transparent);
 
-        return new SolidColorBrush(Colors.Transparent);
+        return status switch
+        {
+            ConversionStatus.Done => GetThemeBrush("SuccessBrush"),
+            ConversionStatus.Failed => GetThemeBrush("DestructiveBrush"),
+            ConversionStatus.Converting => new SolidColorBrush(Color.FromRgb(160, 160, 160)),
+            _ => new SolidColorBrush(Color.FromRgb(100, 100, 100))
+        };
     }
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         => throw new NotImplementedException();
+
+    private static SolidColorBrush GetThemeBrush(string key)
+    {
+        if (Application.Current.Resources[key] is SolidColorBrush brush)
+            return brush;
+        return new SolidColorBrush(Colors.Transparent);
+    }
 }
